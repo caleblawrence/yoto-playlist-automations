28c3d5f53ffd30237e11661c4b76b25d3b7ce7b7
Caleb Lawrence <lawrence.calebc@gmail.com>
2025-04-30 22:41:37 -0500

    collect links from notion

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,14 @@
 const { Client } = require("@notionhq/client");
 import "dotenv/config";
 
-console.log("Starting Notion client...");
-console.log("Environment Variables:");
-console.log(process.env.NOTION_TOKEN);
-
 const notion = new Client({
   auth: process.env.NOTION_TOKEN,
 });
 
 const databaseId = process.env.NOTION_DATABASE_ID;
 
-const getDatabaseData = async () => {
+const getLinks = async () => {
+  const links = [];
   try {
     const response = await notion.databases.query({
       database_id: databaseId,
@@ -23,14 +20,24 @@ const getDatabaseData = async () => {
       },
     });
 
-    console.log("Query Results:", response.results);
-    console.log(
-      "Link:",
-      response.results[0].properties.link.rich_text[0].plain_text
-    );
+    for (const page of response.results) {
+      const link = page.properties.link.rich_text[0].plain_text;
+      links.push(link);
+    }
   } catch (error) {
     console.error("Error querying database:", error);
   }
+
+  return links;
 };
 
-getDatabaseData();
+const fetchAndLogLinks = async () => {
+  try {
+    const links = await getLinks();
+    console.log("Fetched links:", links);
+  } catch (error) {
+    console.error("Error fetching links:", error);
+  }
+};
+
+fetchAndLogLinks();
